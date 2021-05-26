384f090357c39d98c23ce3795045560aeba25593
Chansik Yun <chansigi@gmail.com>
2021-05-26 05:51:06 +0000

    .

--- 4_스위트픽스쳐.cpp
+++ 4_스위트픽스쳐.cpp
@@ -1,9 +1,11 @@
 #include <string>
 
+#include <unistd.h> // sleep
 class Terminal {
 public:
-	void Connect() {}
-	void Disconnect() {}
+	// 만약 Connect() / Disconnect()가 느립니다.
+	void Connect() { sleep(2); }
+	void Disconnect() { sleep(1); }
 
 	void Login(const std::string& id, const std::string& password) {}
 	void Logout() {}
@@ -31,6 +33,34 @@ protected:
 	}
 };
 
+// SetUp() / TearDown() - Per Testcase
+#if 0
+//---- SetUpTestSuite / SetUpTestCase
+TerminalTest* ts = new TerminalTest;
+ts->SetUp();
+ts->LoginTest();
+ts->TearDown();
+delete ts;
+
+TerminalTest* ts = new TerminalTest;
+ts->SetUp();
+ts->LogoutTest();
+ts->TearDown();
+delete ts;
+//---- TearDownTestSuite / TearDownTestCase
+#endif
+
+
+
+// 가정: 만약 픽스쳐 설치와 해체에서 호출되는 함수가 느려서,
+//       "느린 테스트 문제"가 발생하였습니다.
+// 해결방법: 스위트 픽스쳐
+
+// Slow Test
+//  1) 테스트가 너무 느려서, 테스트를 수행하는 개발자의 생산성을 떨어뜨린다.
+//  2) 테스트가 너무 느려서, 아무도 코드가 변경되어도 테스트를 수행하지 않는다.
+
+
 TEST_F(TerminalTest, Login) {
 	term->Login("test_id", "test_password");
 
@@ -43,6 +73,10 @@ TEST_F(TerminalTest, Logout) {
 
 	ASSERT_FALSE(term->IsLogin()) << "로그아웃하였을 때";
 }
+
+TEST_F(TerminalTest, Foo) {}
+TEST_F(TerminalTest, Goo) {}
+
 #if 0
 TEST(TerminalTest, Login) {
 	Terminal* term = new Terminal;
