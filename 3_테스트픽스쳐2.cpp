cfc8427556078d8c1be13cfbb36c7f2d58a3efd8
Chansik Yun <chansigi@gmail.com>
2021-05-26 04:50:11 +0000

    .

--- 3_테스트픽스쳐2.cpp
+++ 3_테스트픽스쳐2.cpp
@@ -17,16 +17,42 @@ public:
 // 1. Delegate Setup(위임 설치)
 //  : 픽스쳐 설치에 관한 코드를 '테스트 유틸리티 함수'를 통해 모듈화한다.
 
-// TEST: 암묵적인 Test Suite 
+#if 0
+TEST(SampleTest, Foo) {} // class SampleTest_Foo_Test : public testing::Test {}
+TEST(SampleTest, Goo) {} // class SampleTest_Goo_Test : public testing::Test {}
+
+//              testing::Test
+//                    |
+//            -------------------
+//            |                 |
+//       SampleTest.Foo     SampleTest.Goo     
+
+
+class SampleTest : public testing::Test {};
+TEST_F(SampleTest, Foo) {}  // class SampleTest_Foo_Test : public SampleTest {};
+TEST_F(SampleTest, Goo) {}  // class SampleTest_Goo_Test : public SampleTest {};
+
+//              testing::Test
+//                    |
+//                SampleTest 
+//                    |
+//            -------------------
+//            |                 |
+//       SampleTest.Foo     SampleTest.Goo     
+#endif
 
 // 적용 방법
 // 1) 명시적인 Test Suite class 를 만들어야 합니다.
 class CalculatorTest : public testing::Test {
-public:
+// public:
+protected:
+// - 자식 클래스로 만들어진 테스트케이스에 접근하기 위해서는 protected로 만들어야 합니다.
 	Calculator* Create() { return new Calculator; }
 };
 
 // 2) TEST가 아닌 TEST_F를 통해 테스트케이스를 만들어야 합니다.
+// TEST: 암묵적인 Test Suite 
+// TEST_F: 명시적인 Test Suite
 TEST_F(CalculatorTest, Plus_2Plus2_Displays4) {
 	// Arrange
 	Calculator* calc = Create();
