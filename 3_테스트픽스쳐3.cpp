d1d83b7e2411abbcb8b0325fe85dd21b63990a95
Chansik Yun <chansigi@gmail.com>
2021-05-26 05:30:30 +0000

    .

--- 3_테스트픽스쳐3.cpp
+++ 3_테스트픽스쳐3.cpp
@@ -20,7 +20,6 @@ public:
 //   단점: 픽스쳐 설치 코드가 테스트 함수 밖에 존재하기 때문에, 테스트 함수만으로 테스트 코드를
 //         이해하기 어려울 수 있다.
 
-
 // xUnit Test Pattern - Test case 구성 방법.
 //  => Four Phase Test Pattern(4단계 테스트 패턴)
 //  1단계: 테스트 픽스쳐를 설치하거나, 실제 결과를 관찰하기 위해서 필요한 것을 집어넣는 작업을 수행한다. - SetUp()
@@ -28,8 +27,27 @@ public:
 //  3단계: 기대 결과를 확인한다.  - TestCase
 //  4단계: 테스트 픽스쳐를 해체해서, 테스트 시작 이전의 상태로 돌려놓는다.
 
+// xUnit Test Framework가 테스트를 수행하는 흐름
+#if 0
+// 신선한 픽스쳐 전략
+CalculatorTest* ts = new CalculatorTest;
+ts->SetUp();
+ts->Foo();
+ts->TearDown();
+delete ts;
+
+CalculatorTest* ts = new CalculatorTest;
+ts->SetUp();
+ts->Goo();
+ts->TearDown();
+delete ts;
+#endif
+
 class CalculatorTest : public testing::Test {
 protected:
+	CalculatorTest() { printf("CalculatorTest()\n"); }
+	~CalculatorTest() { printf("~CalculatorTest()\n"); }
+
 	// virtual void SetUp() {}
 	// void SetUp() {}
 	void SetUp() override {
