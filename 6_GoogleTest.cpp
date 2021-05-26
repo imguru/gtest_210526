1e7d5741840462176ae16d614cae6d6d103baae4
Chansik Yun <chansigi@gmail.com>
2021-05-26 07:11:41 +0000

    .

--- 6_GoogleTest.cpp
+++ 6_GoogleTest.cpp
@@ -13,10 +13,12 @@ struct User {
 	int GetAge() const { return age; }
 };
 
-int foo() { return 420; }
-int goo() { return 420; }
-User* hoo() { return nullptr; }
+int foo() { return 42; }
+int goo() { return 42; }
+// User* hoo() { return nullptr; }
+User* hoo() { return new User(); }
 
+// 1. EXPECT_
 // ASSERT_EQ / NE / LT / GT / LE / GE / TRUE / FALSE ...
 // => 단언문이 실패할 경우 이후의 코드를 수행하지 않습니다.
 // => 하나의 테스트 함수 안에서, 여러개의 단언문을 사용하는 좋지 않다.
@@ -25,7 +27,6 @@ User* hoo() { return nullptr; }
 // EXPECT_EQ / NE / LT / GT / LE / GE / TRUE / FALSE ...
 // => 단언문이 실패할 경우, 테스트의 결과는 실패가 되지만 이후의 코드는 수행됩니다.
 //  : 죽은 단언문 / 중복된 테스트케이스
-
 TEST(GoogleTest, Sample1) {
 	int expected = 42;
 
@@ -35,12 +36,33 @@ TEST(GoogleTest, Sample1) {
 
 	ASSERT_NE(user, nullptr);
 	EXPECT_EQ(expected, user->GetAge()) << "Reason User";
-
 	EXPECT_EQ(expected, actual1) << "Reason 1";
 	EXPECT_EQ(expected, actual2) << "Reason 2";  // 죽은 단언문
 	// ASSERT_EQ(expected, actual1) << "Reason 1";
 	// ASSERT_EQ(expected, actual2) << "Reason 2";  // 죽은 단언문
 }
+
+// 2. 문자열 단언문
+//   std::string
+//    - EXPECT_EQ / EXPECT_NE
+//   const char* 
+//    - EXPECT_STREQ/STRNE          : strcmp
+//    - EXPECT_STRCASEQ/STRCASENE   : strcmpi
+TEST(GoogleTest, Sample2) {
+	std::string s1 = "hello";
+	std::string s2 = "hello";
+	const char* s3 = s1.c_str();
+	const char* s4 = "HEllo";
+
+	EXPECT_EQ(s1, s2);
+	// EXPECT_STREQ(s3, s4);
+	EXPECT_STRCASEEQ(s3, s4);
+}
+
+
+
+
+
 
 
 
