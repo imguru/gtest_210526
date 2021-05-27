bbfed1a81d5b4db98363f059ab9a2d269cef29b5
Chansik Yun <chansigi@gmail.com>
2021-05-27 00:34:42 +0000

    .

--- 7_비기능테스트.cpp
+++ 7_비기능테스트.cpp
@@ -11,6 +11,8 @@ void OpenFile(const std::string& filename) {
 //-----
 #include <gtest/gtest.h>
 
+
+#if 0
 // OpenFile이 1초 안에 수행되는지 검증하고 싶다.
 //  방법 1. TestSuite
 class SampleTest : public testing::Test {
@@ -30,6 +32,36 @@ protected:
 TEST_F(SampleTest, OpenFile) {
 	OpenFile("a.txt");
 }
+#endif
+//  방법 2. 사용자 정의 단언문을 제공합니다.
+//   => 매크로를 만들어봅니다.
+#define EXPECT_TIMEOUT(fn, sec)                           					  \
+	do {               									  					  \
+		time_t startTime = time(nullptr);					 				  \
+		fn;													  				  \
+		time_t duration = time(nullptr) - startTime;          				  \
+		EXPECT_LE(duration, sec) << "시간 초과: " << duration << " 초 걸림";  \
+	} while (0)
+
+TEST(SampleTest, OpenFile) {
+	EXPECT_TIMEOUT(OpenFile("a.txt"), 1);
+}
+
+#if 0
+TEST(SampleTest, OpenFile) {
+	time_t startTime = time(nullptr);
+	OpenFile("a.txt");
+	time_t duration = time(nullptr) - startTime;
+	EXPECT_LE(duration, 1) << "시간 초과: " << duration << " 초 걸림";
+}
+#endif
+
+
+
+
+
+
+
 
 
 #if 0
