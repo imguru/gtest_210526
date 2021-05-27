f0bcf9effe36b76605faeef59071b83b68abac3f
Chansik Yun <chansigi@gmail.com>
2021-05-27 07:11:34 +0000

    .

--- 16_Mock.cpp
+++ 16_Mock.cpp
@@ -47,12 +47,22 @@ public:
 #include <string>
 #include <algorithm>  // find
 
-
-// Test Spy Pattern
+// Mock Object Pattern
 //   의도: SUT의 함수를 호출하였을 때 발생하는 부수효과를 관찰할 수 없어서, 테스트되지 않은 
 //         요구사항이 있다.
-//   방법: 목격한 일을 기록해두었다가 나중에 테스트에서 확인할 수 있도록 만들어진 테스트 대역
-//         "다른 컴포넌트로부터의 간접 출력을 저장해두었다가 검증한다."
+//   방법: 모의 객체(Mock Object)를 이용해서 "행위 기반 검증"을 수행한다.
+
+//   행위 기반 검증(동작 검증)
+//     - 객체에 작용을 가한후, 내부적으로 발생하는 함수의 호출 여부 / 횟수 / 순서를 통해 검증한다.
+//   상태 기반 검증(상태 검증)
+//     - 객체에 작용을 가한후, 내부적으로 발생하는 부수효과(값의 변경)을 통해 검증한다.
+
+//   모의 객체(Mock Object)
+//    - 내부적으로 발생하는 함수의 호출 여부 / 횟수 / 순서 등의 정보를 저장합니다.
+//    => Mock Framework
+//      Java: jMock, EasyMock, Mockito, Spock ...
+//       C++: Google Mock
+
 class SpyTarget : public DLoggerTarget {
 	std::vector<std::string> history;
 
