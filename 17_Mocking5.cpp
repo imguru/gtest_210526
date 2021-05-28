51a5ba5a2489fdef8b5f5ab3e20df362c3ad5fe9
Chansik Yun <chansigi@gmail.com>
2021-05-28 01:26:44 +0000

    .

--- 17_Mocking5.cpp
+++ 17_Mocking5.cpp
@@ -2,44 +2,36 @@
 #include <stdio.h>
 
 // Dependency Injection
-//  1) 인터페이스 / 추상 클래스
+//  2) Template - Policy Based Design(단위 전략)
+//    => 암묵적인 인터페이스
 struct Packet {};
-class PacketStream {
+class ConcreatePacketStream {
 public:
-	virtual ~PacketStream() {}
-
-	virtual void AppendPacket(Packet* new_packet) = 0;
-	virtual const Packet* GetPacket(size_t packet_number) const = 0;
-};
-
-class ConcreatePacketStream : public PacketStream {
-public:
-	void AppendPacket(Packet* new_packet) override {
+	void AppendPacket(Packet* new_packet) {
 		printf("ConcreatePacketStream::AppendPacket\n");
 	}
 
-	const Packet* GetPacket(size_t packet_number) const override {
+	const Packet* GetPacket(size_t packet_number) const {
 		printf("ConcreatePacketStream::GetPacket\n");
 		return nullptr;
 	}
 };
 
-
 class PacketReader {
 public:
+	template <typename PacketStream>
 	void ReadPacket(PacketStream* stream, size_t packet_number) {
 		stream->AppendPacket(nullptr);
-		stream->GetPacket(packet_number);
+		// stream->GetPacket(packet_number);
 	}
 };
 
-//-------
 #include <gmock/gmock.h>
 
-class MockPacketStream : public PacketStream {
+class MockPacketStream {
 public:
-	MOCK_METHOD(void, AppendPacket, (Packet* new_packet), (override));
-	MOCK_METHOD(const Packet*, GetPacket, (size_t packet_number), (const, override));
+	MOCK_METHOD(void, AppendPacket, (Packet* new_packet));
+	MOCK_METHOD(const Packet*, GetPacket, (size_t packet_number), (const));
 };
 
 TEST(PacketReaderTest, ReadPacket) {
@@ -60,8 +52,6 @@ int main() {
 	reader.ReadPacket(&stream, 42);
 }
 #endif
-
-
 
 
 
