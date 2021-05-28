05e0dd51e06dadd085d9bf87f261dfed6c88cf1c
Chansik Yun <chansigi@gmail.com>
2021-05-28 00:22:08 +0000

    .

--- 17_Mocking.cpp
+++ 17_Mocking.cpp
@@ -8,6 +8,9 @@ public:
 
 	virtual void Play() = 0;
 	virtual void Stop(int n) = 0;
+
+	virtual std::string GetName() const = 0;
+	virtual void Foo() noexcept = 0;
 };
 
 //------
@@ -15,9 +18,15 @@ public:
 
 // 한정자
 // 1. override: virtual 함수 - Optional
-
+// 2. const: Required
+// 3. noexcept: Required
 class MockMP3 : public MP3 {
 public:
+	// std::string GetName() const override {}
+	MOCK_METHOD(std::string, GetName, (), (const, override));
+	// void Foo() noexcept override {}
+	MOCK_METHOD(void, Foo, (), (noexcept, override));
+
 	// void Play() override {}
 	MOCK_METHOD(void, Play, (), (override));
 	// void Stop(int n) override {}
