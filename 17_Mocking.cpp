81c9745cc374b94ad3a5e1fb9310de9ea0f5fee0
Chansik Yun <chansigi@gmail.com>
2021-05-28 00:30:05 +0000

    .

--- 17_Mocking.cpp
+++ 17_Mocking.cpp
@@ -11,6 +11,9 @@ public:
 
 	virtual std::string GetName() const = 0;
 	virtual void Foo() noexcept = 0;
+
+	virtual std::pair<bool, int> GetPair() const = 0;
+	virtual bool CheckMap(std::map<int, double> a, bool b) = 0;
 };
 
 //------
@@ -20,8 +23,22 @@ public:
 // 1. override: virtual 함수 - Optional
 // 2. const: Required
 // 3. noexcept: Required
+
+// Type Aliasing
+using BoolIntPair = std::pair<bool, int>;
+using IntDoubleMap = std::map<int, double>;
+
 class MockMP3 : public MP3 {
 public:
+	// std::pair<bool, int> GetPair() const override {}
+	//  - 템플릿 타입 인자가 2개 이상 존재할 경우, 괄호가 필요합니다.
+	// MOCK_METHOD((std::pair<bool, int>), GetPair, (), (const, override));
+	// MOCK_METHOD(bool, CheckMap, ((std::map<int, double> a), bool b), (override));
+	
+	// - Type Alising
+	MOCK_METHOD(BoolIntPair, GetPair, (), (const, override));
+	MOCK_METHOD(bool, CheckMap, (IntDoubleMap a, bool b), (override));
+
 	// std::string GetName() const override {}
 	MOCK_METHOD(std::string, GetName, (), (const, override));
 	// void Foo() noexcept override {}
