749718247614739a14dd99279629c0f8e7d46a09
Chansik Yun <chansigi@gmail.com>
2021-05-28 05:31:42 +0000

    .

---
+++ 21_동작검증2.cpp
@@ -0,0 +1,69 @@
+
+#include <stdio.h>
+
+class Calc {
+public:
+	virtual ~Calc() {}
+
+	virtual int Add(int a, int b) = 0;
+	virtual int Sub(int a, int b) = 0;
+};
+
+void Calculate(Calc* p) {
+	// printf("%d\n", p->Add(10, 20));
+	if (p->Add(10, 20) == 30) {
+		printf("%d\n", p->Sub(30, 20));
+	}
+}
+
+#include <gmock/gmock.h>
+
+class MockCalc : public Calc {
+public:
+	MOCK_METHOD(int, Add, (int a, int b), (override));
+	MOCK_METHOD(int, Sub, (int a, int b), (override));
+};
+
+// ON_CALL / EXPECT_CALL을 동시에 사용하고 있다면,
+//  => EXPECT_CALL을 통해서 ON_CALL 역활을 대신할 수 있습니다.
+
+// ON_CALL
+//  - WillByDefault
+
+// EXPECT_CALL
+
+using testing::Return;
+TEST(CalcTest, Calculate) {
+	MockCalc mock;
+
+	EXPECT_CALL(mock, Add)
+		.WillOnce(Return(30));
+
+	EXPECT_CALL(mock, Sub)
+		.WillOnce(Return(10000));
+
+	Calculate(&mock);
+}
+#if 0
+TEST(CalcTest, Calculate) {
+	MockCalc mock;
+	ON_CALL(mock, Add).WillByDefault(Return(30));
+
+	EXPECT_CALL(mock, Add);
+	EXPECT_CALL(mock, Sub);
+
+	Calculate(&mock);
+}
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
