53703fedf5715e1f9ac7b9865bc26f791ff2edb4
Chansik Yun <chansigi@gmail.com>
2021-05-28 04:31:55 +0000

    .

--- 21_동작검증.cpp
+++ 21_동작검증.cpp
@@ -34,15 +34,47 @@ TEST(UserTest, Sample1) {
 	Sample1(&mock);
 }
 
-// 2. 함수 호출 횟수
+void Sample2(User* p) {
+	p->Go(10, 121);
+	p->Go(11, -42);
+	p->Go(12, -53);
+	p->Go(13, 114);
+	p->Go(14, 0);
+}
+
+// 2. 함수 호출 횟수(인자 매칭(Matcher)에 따라서 판단되는 횟수가 달라집니다)
 //   : EXPECT_CALL(..).Times(...)
+
+using testing::_;   // 모든 것과 매칭
+using testing::Gt;  // >
+using testing::Eq;  // ==
+using testing::Ne;  // !=
+using testing::Lt;  // <
+using testing::Ge;  // >=
+using testing::Le;  // <=
+using testing::AllOf; // &&
+using testing::AnyOf; // ||
+using testing::Matcher;
 TEST(UserTest, Sample2) {
 	MockUser mock;
 
-	EXPECT_CALL(mock, Go).Times(2);
+	// EXPECT_CALL(mock, Go).Times(4);
+	// EXPECT_CALL(mock, Go(Eq(10), Gt(20))).Times(4);
+	// EXPECT_CALL(mock, Go(Eq(10), _)).Times(4);
+	
+	// arg#1: arg1 >= 10 && arg1 < 20
+	// arg#2: arg2 > 100 || arg2 <= 0
+	Matcher<int> arg1 = AllOf(Ge(10), Lt(20));
+	Matcher<int> arg2 = AnyOf(Gt(100), Le(0));
+	EXPECT_CALL(mock, Go(arg1, arg2)).Times(5);
 
-	Sample1(&mock);
+	Sample2(&mock);
 }
+
+
+
+
+
 
 
 
